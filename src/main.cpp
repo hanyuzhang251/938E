dbb0546e26a40cc215533b3d154defe86aab58a0
Hanyu Zhang <hanyuzhang251@gmail.com>
2024-11-17 13:05:31 -0800

    aegwrshtdu

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include "main.h"
 #include "lemlib/api.hpp"
+#include "lemlib/asset.hpp"
 #include "lemlib/chassis/chassis.hpp"
 #include "lemlib/chassis/trackingWheel.hpp"
+#include "pros/adi.hpp"
 #include "pros/misc.h"
 #include <cstddef>
 
@@ -64,7 +66,7 @@ pros::MotorGroup intake_both ({
 	INTAKE_TOP_PORT
 });
 
-pros::adi::AnalogOut mogo_clamp_piston (MOGO_CLAMP_PORT);
+pros::ADIAnalogOut mogo_clamp_piston (MOGO_CLAMP_PORT);
 pros::adi::AnalogOut mogo_bar_piston (MOGO_BAR_PORT);
 
 pros::adi::AnalogOut arm (ARM_PORT);
@@ -73,7 +75,7 @@ pros::adi::AnalogOut arm_end (ARM_END_PORT);
 pros::IMU imu (IMU_PORT);
 
 
-lemlib::Drivetrain drivetrain(&drive_train_left_motor_group, &drive_train_right_motor_group, 11, lemlib::Omniwheel::NEW_275, 450, 2);
+lemlib::Drivetrain drivetrain(&drive_train_left_motor_group, &drive_train_right_motor_group, 11, lemlib::Omniwheel::NEW_275, 600, 2);
 
 lemlib::OdomSensors sensors (nullptr, nullptr, nullptr, nullptr, &imu);
 
@@ -133,43 +135,45 @@ void initialize() {
 
 void disabled() {}
 
-void competition_initialize() {
+void competition_initialize() {	
 	arm.set_value(true);
 }
 
-ASSET(rightGrabMogo_txt)
+ASSET(test_txt)
+ASSET(rightGrabRings_txt)
 
 void autonomous() {
-	// // intake_top_motor.move(100);
-	// // pros::delay(1000);
-	// // intake_top_motor.brake();
-	// chassis.setPose(47, 0, 0);
-	// chassis.moveToPoint(23, 47, 5000);
-	// // arm.set_value(true);
-	// // chassis.moveToPose(62, 10, 310, 1000, {}, false);
-	// // arm.set_value(false);
-	// // chassis.moveToPose(30, 18, 315, 5000, {}, false);
-	// // mogo_clamp_piston.set_value(true);
-	// // intake_bottom_motor.move(100);
-	// // chassis.moveToPose(28, 42, 140, 5000, {}, false);
+	chassis.setPose(61.256, 9.955, 90);
 
-	chassis.setPose(59, 14, 145);
+	// arm.set_value(false);
+	// pros::delay(250);
+	// arm_end.set_value(true);
+	// pros::delay(250);
+	// arm.set_value(true);
+	// pros::delay(250);
 
-	arm.set_value(false); // intentional
-	pros::delay(250);
-	arm_end.set_value(true);
-	pros::delay(250);
-	arm.set_value(true);
-	pros::delay(250);
+	chassis.moveToPoint(49.045, 9.955, 5000, {.forwards=false}, false);
 
-	chassis.follow(rightGrabMogo_txt, 10, 500, false, true);
-	chassis.waitUntil(34);
-	mogo_clamp_piston.set_value(true);
-	pros::delay(250);
+	// chassis.follow(test_txt, 10, 10000, true, false);
+	
+	// chassis.waitUntil(34);
+	// mogo_clamp_piston.set_value(true);
+	// pros::delay(250);
 
-	chassis.turnToPoint(23.5, 47, 5000, {}, false);
-	intake_both.move(100);
-	chassis.moveToPoint(26.5, 40.5, 5000, false);
+	// chassis.turnToPoint(23.5, 47, 5000, {}, false);
+	// intake_both.move(100);
+	// chassis.moveToPoint(26.5, 40.5, 5000, {}, false);
+
+	// chassis.turnToPoint(5, 32.5, 5000, {}, false);
+	// intake_both.brake();
+
+	// chassis.follow(rightGrabMogo_txt, 10, 5000);
+
+	// chassis.waitUntil(12);
+	// intake_both.move(100);
+
+	// chassis.waitUntilDone();
+	// intake_both.brake();
 }
 
 void opcontrol() {
