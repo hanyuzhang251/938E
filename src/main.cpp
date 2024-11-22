4f1e556181f475edab3ddc1e31f91c5667ad1dce
Hanyu Zhang <hanyuzhang251@gmail.com>
2024-11-21 16:25:17 -0800

    fix things

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,10 @@
 #include <cstddef>
 #include <queue>
 #include <atomic>
+#include <string>
+
+#define digi_button controller_digital_e_t
+#define anal_button controller_analog_e_t
 
 constexpr double INCH_PER_G = 386.08858267717;
 
@@ -36,27 +40,29 @@ constexpr int IMU_PORT = 21;
 constexpr int INTAKE_MOTOR_SPEED = 127;
 
 constexpr int ARM_SPEED = 127;
-constexpr int ARM_IDLE_SPEED = 30;
+constexpr int ARM_IDLE_SPEED = 5;
 
 // config controller
 
-constexpr pros::controller_analog_e_t DRIVE_JOYSTICK = pros::E_CONTROLLER_ANALOG_LEFT_Y;
-constexpr pros::controller_analog_e_t TURN_JOYSTICK = pros::E_CONTROLLER_ANALOG_RIGHT_X;
+constexpr std::pair<pros::digi_button, pros::digi_button> INFO_BUTTONS ({pros::E_CONTROLLER_DIGITAL_R1, pros::E_CONTROLLER_DIGITAL_R2});
 
-constexpr pros::controller_digital_e_t INTAKE_FWD_BUTTON = pros::E_CONTROLLER_DIGITAL_L2;
-constexpr pros::controller_digital_e_t INTAKE_REV_BUTTON = pros::E_CONTROLLER_DIGITAL_L1;
+constexpr pros::anal_button DRIVE_JOYSTICK = pros::E_CONTROLLER_ANALOG_LEFT_Y;
+constexpr pros::anal_button TURN_JOYSTICK = pros::E_CONTROLLER_ANALOG_RIGHT_X;
 
-constexpr pros::controller_digital_e_t MOGO_ON_BUTTON = pros::E_CONTROLLER_DIGITAL_A;
-constexpr pros::controller_digital_e_t MOGO_OFF_BUTTON = pros::E_CONTROLLER_DIGITAL_B;
+constexpr pros::digi_button INTAKE_FWD_BUTTON = pros::E_CONTROLLER_DIGITAL_L2;
+constexpr pros::digi_button INTAKE_REV_BUTTON = pros::E_CONTROLLER_DIGITAL_L1;
 
-constexpr pros::controller_digital_e_t BAR_ON_BUTTON = pros::E_CONTROLLER_DIGITAL_X;
-constexpr pros::controller_digital_e_t BAR_OFF_BUTTON = pros::E_CONTROLLER_DIGITAL_Y;
+constexpr pros::digi_button MOGO_ON_BUTTON = pros::E_CONTROLLER_DIGITAL_A;
+constexpr pros::digi_button MOGO_OFF_BUTTON = pros::E_CONTROLLER_DIGITAL_B;
 
-constexpr pros::controller_digital_e_t ARM_UP_BUTTON = pros::E_CONTROLLER_DIGITAL_UP;
-constexpr pros::controller_digital_e_t ARM_DOWN_BUTTON = pros::E_CONTROLLER_DIGITAL_DOWN;
+constexpr pros::digi_button BAR_ON_BUTTON = pros::E_CONTROLLER_DIGITAL_X;
+constexpr pros::digi_button BAR_OFF_BUTTON = pros::E_CONTROLLER_DIGITAL_Y;
 
-constexpr pros::controller_digital_e_t ARM_END_ON_BUTTON = pros::E_CONTROLLER_DIGITAL_RIGHT;
-constexpr pros::controller_digital_e_t ARM_END_OFF_BUTTON = pros::E_CONTROLLER_DIGITAL_LEFT;
+constexpr pros::digi_button ARM_UP_BUTTON = pros::E_CONTROLLER_DIGITAL_UP;
+constexpr pros::digi_button ARM_DOWN_BUTTON = pros::E_CONTROLLER_DIGITAL_DOWN;
+
+constexpr pros::digi_button ARM_END_ON_BUTTON = pros::E_CONTROLLER_DIGITAL_RIGHT;
+constexpr pros::digi_button ARM_END_OFF_BUTTON = pros::E_CONTROLLER_DIGITAL_LEFT;
 
 // config drive
 
@@ -132,6 +138,18 @@ void get_robot_position(long last_update) {
 constexpr size_t TELEMTRY_SIZE = 100;
 std::string telemetry[TELEMTRY_SIZE];
 
+void update_telemetry() {
+	if (master.get_digital(INFO_BUTTONS.first) && master.get_digital(INFO_BUTTONS.second)) {
+		master.set_text(0, 0, telemetry[0]);
+		master.set_text(1, 0, telemetry[1]);
+		master.set_text(2, 0, telemetry[2]);
+	} else {
+		master.set_text(0, 0, telemetry[0]);
+		master.set_text(1, 0, telemetry[1]);
+		master.set_text(2, 0, telemetry[2]);
+	}
+}
+
 void log(std::string str) {
 	for (int i = TELEMTRY_SIZE - 1; i > 0; --i) {
 		telemetry[i] = telemetry[i - 1];
@@ -139,34 +157,34 @@ void log(std::string str) {
 
 	telemetry[0] = str;
 
-	master.set_text(0, 0, telemetry[0]);
-	master.set_text(1, 0, telemetry[1]);
-	master.set_text(2, 0, telemetry[2]);
+	update_telemetry();
 }
 
-void amend(std::string str) {
+void amend_last_log(std::string str) {
 	telemetry[0] = str;
 
 	master.set_text(0, 0, telemetry[0]);
+
+	update_telemetry();
 }
 
 // competition:
 
 void initialize() {
-	log("initializing");
+	log("initialize");
 
 	pros::lcd::initialize();
     
-	log("calibrating imu");
+	log("reset imu");
 	auto start = pros::millis();
 
 	imu.reset();
 	while(imu.is_calibrating()) {
-		amend("calibrating imu " + std::to_string(pros::millis()));
+		amend_last_log("reset imu " + std::to_string(pros::millis()));
 		pros::delay(PROCESS_DELAY);
 	}
 
-	amend("imu calibrated " +  std::to_string(pros::millis()));
+	amend_last_log("imu reset " +  std::to_string(pros::millis()));
 
     pros::Task pos_tracking_task([&]() {
 		last_pos_update = pros::millis();
@@ -201,13 +219,32 @@ struct Pose {
 
 std::queue<Pose> instructions;
 
-void auton_async() {
-	log("async auton started");
-
-	Pose target;
+void adjust_angle(Pose target) {
+	log("adj. angle from " + std::to_string(head) + " to " + std::to_string(target.head));
 
 	int error = 0;
 	int integral = 0;
+
+	while(true) {
+		float right_side_error = std::abs(target.head - head.load());
+		float left_side_error = std::abs(std::min(target.head, head.load()) + 360 - std::max(target.head, head.load()));
+
+		if (right_side_error < left_side_error) error = right_side_error;
+		else error = -left_side_error;
+
+		if (std::abs(error) >= angular_pid.small_error) {
+			float calc_power = calcPowerPID(error, integral, angular_pid);
+
+			dt_left_motors.move(calc_power);
+			dt_right_motors.move(-calc_power);
+		}
+	}
+}
+
+void auton_async() {
+	log("async auton start");
+
+	Pose target;
 
 	while (true) {
 		pros::delay(PROCESS_DELAY);
@@ -216,21 +253,18 @@ void auton_async() {
 
 		Pose target = instructions.front();
 
-	
-		float right_side_error = std::abs(target.head - head.load());
-		float left_side_error = std::abs(std::min(target.head, head.load()) + 360 - std::max(target.head, head.load()));
-		if (error >= angular_pid.small_error) {
-			dt_left_motors.move(calcPowerPID(error, integral, angular_pid));
-		}
+		adjust_angle(target);
 	}
 }
 
 void autonomous() {
-	log("auton stated");
+	log("auton start");
 
 	pros::Task auton_task(auton_async);
 
+	log("end async auton");
 	auton_task.remove();
+	amend_last_log("async auton end");
 }
 
 float calcPowerCurve(int value, int other_value, DriveCurve curve, int ratio, int other_ratio) {
@@ -268,7 +302,7 @@ void opcontrol() {
 
 		// intake
 		if (master.get_digital(INTAKE_FWD_BUTTON)) intake_motor.move(INTAKE_MOTOR_SPEED);
-		else if (master.get_digital(pros::E_CONTROLLER_DIGITAL_L1)) intake_motor.move(-INTAKE_MOTOR_SPEED);
+		else if (master.get_digital(INTAKE_REV_BUTTON)) intake_motor.move(-INTAKE_MOTOR_SPEED);
 		else intake_motor.brake();
 		
 		// mogo
