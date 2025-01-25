6b3bcdd08265c8cd33c7d063d6aaa23a6ba90821
Hanyu Zhang <hanyuzhang251@gmail.com>
2025-01-25 11:55:07 -0800

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -624,11 +624,11 @@ void autonomous() {
 	target_heading.store(-90);
 	wait(680);
 
-	mogo.set_value(true);
-	wait(250);
-
 	target_dist.fetch_add(-17);
 	wait(800);
+
+	mogo.set_value(true);
+	wait(250);
 
 	target_heading.store(20);
 	wait(680);
@@ -695,14 +695,16 @@ void autonomous() {
 	wait(900);
 
 	lateral_pid_process.max_speed = 127;
-	target_dist.fetch_add(36);
+	target_dist.fetch_add(48);
 	wait(150);
 
 	target_heading.store(0);
 	wait(900);
 
-	target_dist.fetch_add(-15);
+	target_dist.fetch_add(-23);
 	wait(800);
+
+	wait(5000);
 
 	intake.brake();
 	mogo.set_value(false);
