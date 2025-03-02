802aae299e9e752b8a1671e5cfcc75d8fb8299ae
Hanyu Zhang <hanyuzhang251@gmail.com>
2025-03-02 12:56:47 -0800

    curves

--- include/chisel/drive.h
+++ include/chisel/drive.h
@@ -5,6 +5,16 @@
 
 namespace chisel {
 
+constexpr auto linear_curve = [](float input) {return input;};
+constexpr auto sigmoid_curve = [](float input) {
+    // finer control at high speeds
+    return 127 * std::sin((M_PI * input) / 254);
+};
+constexpr auto sigma_curve = [](float input) {
+    // finer control at low speeds
+    return 254 * 1 / M_PI / std::arcsin(input / 127);
+};
+
 struct DriveCurve {
     float deadband;
     float min_out;
