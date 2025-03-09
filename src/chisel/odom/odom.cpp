cdf054ab68a13ebd9a3996509c52a4acfb5a9eef
Hanyu Zhang <hanyuzhang251@gmail.com>
2025-03-08 22:41:09 -0800

    odom logs

--- src/chisel/odom/odom.cpp
+++ src/chisel/odom/odom.cpp
@@ -12,6 +12,8 @@ Pose solve_imu_bias(pros::Imu inertial, int32_t timeout) {
 
     int ticks = 0;
 
+    printf("%sstart solving imu bias\n", prefix());
+
     while (pros::millis() < end) {
         bx.fetch_add(inertial.get_accel().x);
         by.fetch_add(inertial.get_accel().y);
@@ -24,6 +26,8 @@ Pose solve_imu_bias(pros::Imu inertial, int32_t timeout) {
     bx.store(bx.load() / ticks);
     by.store(by.load() / ticks);
 
+    printf("%ssolved imu bias for %d ticks, result: bx=%f, by=%f\n", prefix(), ticks, bx.load(), by.load());
+
     return Pose(bx.load(), by.load(), bh.load());
 }
 
@@ -31,7 +35,9 @@ TrackingWheel::TrackingWheel(
     pros::Rotation* rotation_sensor, Pose offset,
     float wheel_size)
     : rotation_sensor(rotation_sensor), offset(offset),
-    wheel_size(wheel_size) {};
+    wheel_size(wheel_size) {
+        printf("%screate new TrackingWheel: offset=(%f, %f, %f), wheel_size=%f\n", prefix(), offset.x.load(), offset.y.load(), offset.h.load(), wheel_size);
+    };
 
 TrackingSetup::TrackingSetup(
     TrackingWheel* tracking_wheel_list_ptr,
