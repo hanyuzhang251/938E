2a399546880c09e3722a473bf88f90fa05c053f6
Hanyu Zhang <hanyuzhang251@gmail.com>
2025-03-08 23:06:36 -0800

    update odom logs

--- src/chisel/odom/odom.cpp
+++ src/chisel/odom/odom.cpp
@@ -36,7 +36,7 @@ TrackingWheel::TrackingWheel(
     float wheel_size)
     : rotation_sensor(rotation_sensor), offset(offset),
     wheel_size(wheel_size) {
-        printf("%screate new TrackingWheel: offset=(%f, %f, %f), wheel_size=%f\n", prefix(), offset.x.load(), offset.y.load(), offset.h.load(), wheel_size);
+        printf("%screate new TrackingWheel: sensor(%d), offset=(%f, %f, %f), wheel_size=%f\n", prefix(), rotation_sensor->get_port(), offset.x.load(), offset.y.load(), offset.h.load(), wheel_size);
     };
 
 TrackingSetup::TrackingSetup(
@@ -46,6 +46,8 @@ TrackingSetup::TrackingSetup(
     tracking_wheel_list.reserve(tracking_wheel_count);
     tracking_wheel_list.insert(tracking_wheel_list.end(),
         tracking_wheel_list_ptr, tracking_wheel_list_ptr + tracking_wheel_count);
+
+    printf("%screate new TrackingSetup: tracking_wheel_count=%d\n", prefix(), tracking_wheel_count);
 }
 
 } // namespace chisel
