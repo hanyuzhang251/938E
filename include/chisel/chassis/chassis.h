2e7c769636c7ed0bc8be664ac1222567207b9c98
Hanyu Zhang <hanyuzhang251@gmail.com>
2025-03-12 22:18:01 -0700

    movements

--- include/chisel/chassis/chassis.h
+++ include/chisel/chassis/chassis.h
@@ -3,12 +3,17 @@
 #include "main.h"
 #include "chisel/chassis/drive.h"
 #include "chisel/odom/odom.h"
+#include "chisel/chassis/movement/movement.h"
+
+#include <queue>
 
 namespace chisel {
 
 struct Chassis {
     DriveTrain *drive_train;
     Odom *odom;
+
+    std::queue<Movement> instruction_queue;
 };
 
 } // namespace chisel
---
+++ include/chisel/chassis/movement/movement.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "chisel/data/pose.h"
+
+namespace chisel {
+
+class Movement {
+public:
+    virtual void setup(Pose dest, bool async, float* args, int n_args) = 0;
+    virtual bool is_async() = 0;
+
+    virtual void pull_pose(Pose new_pose) = 0;
+
+    virtual void update() = 0;
+    virtual std::pair<float, float> get_controls();
+
+    virtual ~Movement() = default;
+};
+
+} // namespace chisel
