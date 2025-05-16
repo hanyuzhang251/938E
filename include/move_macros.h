d1a62518ea71f4c3b2dd5c46f6bb7b8cec379380
Hanyu Zhang <hanyuzhang251@gmail.com>
2025-05-16 12:39:18 -0700

    remove chassis

--- include/move_macros.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#define MOVE_FORWARD(dist, min_speed, max_speed) \
-    lateral_pid_controller.max_speed = (max_speed); \
-    lateral_pid_controller.min_speed = (min_speed); \
-    target_dist.store((dist)); \
-    wait_stable(lateral_pid_controller, (dist));
